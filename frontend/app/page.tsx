1d6b0e47c4df40eaad957b9e52591d986ac41105
minh-p <minhisverynormal@gmail.com>
2023-09-01 16:33:48 -0700

    Basic front end setup

--- frontend/app/page.tsx
+++ frontend/app/page.tsx
@@ -1,9 +1,68 @@
+"use client";
+
 const Home = () => {
   return (
-    <div>
-      <h1>Hello</h1>
-    </div>
-  )
-}
+    <section>
+      <article className="max-w-[800px] m-auto p-5">
+        <h1>Test</h1>
+        <p>
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam orci
+          mauris, hendrerit in lacus eu, ornare gravida lectus. Proin maximus
+          ullamcorper varius. Pellentesque et fermentum tellus, id porta justo.
+          In eget metus eu purus lobortis vulputate a sed tellus. Donec ut magna
+          vel libero porttitor condimentum. Proin tempus orci vitae placerat
+          porttitor. Aliquam erat volutpat. Sed eu dui ac lacus egestas feugiat.
+          Sed eget turpis sodales, egestas justo in, fringilla orci. Vivamus
+          fermentum vehicula libero, vitae egestas leo suscipit ut. Maecenas
+          rhoncus eros et eros gravida, euismod placerat dui cursus. Donec
+          ultrices lacus quis quam blandit rutrum. Sed pellentesque molestie
+          blandit. Phasellus hendrerit turpis eu ex eleifend, ut egestas nisl
+          pellentesque. Phasellus sed dictum nisi. In ac luctus nulla, eu
+          malesuada urna. Curabitur convallis ullamcorper urna id vulputate.
+          Mauris commodo, elit in convallis cursus, quam augue hendrerit risus,
+          et vestibulum felis eros sit amet tellus. Vivamus eget elit feugiat,
+          placerat libero vitae, mattis ipsum. Proin quis mauris vitae ipsum
+          auctor efficitur at vitae turpis. Nullam ipsum est, maximus at blandit
+          at, consectetur vel metus. Aliquam erat volutpat. In elit dolor,
+          blandit sed ullamcorper in, consectetur nec risus. Sed consectetur
+          arcu eget mi elementum, in convallis augue egestas. Duis rhoncus
+          lectus eget elit commodo commodo. Vivamus eget gravida mi. Sed
+          fringilla aliquam nunc quis molestie. Suspendisse vel lorem nec nisl
+          tempor luctus sit amet ut purus. Praesent sed ultricies lectus. Nulla
+          in sollicitudin justo, eget porta elit. Vestibulum tempor ligula et
+          lacus vehicula rutrum. Suspendisse finibus erat vitae magna ultricies,
+          vitae dictum ipsum aliquet. Sed blandit accumsan nibh, non dapibus
+          ligula commodo id. Donec egestas ac felis interdum iaculis. Interdum
+          et malesuada fames ac ante ipsum primis in faucibus. Aliquam pharetra
+          tincidunt feugiat. Phasellus rutrum ullamcorper imperdiet. Integer
+          tempor sit amet lorem vel venenatis. Fusce finibus quam diam, a
+          viverra eros sodales in. Aenean ac feugiat nulla. Pellentesque et
+          iaculis dolor. Mauris sollicitudin in erat quis tempor. Aliquam
+          iaculis ut nisi at ultricies. Quisque vitae nisl elit. Nulla gravida
+          nunc eu lectus faucibus, eget aliquet sapien mollis. Fusce eget
+          euismod lectus. Pellentesque habitant morbi tristique senectus et
+          netus et malesuada fames ac turpis egestas. Vivamus suscipit dapibus
+          leo, non convallis dui laoreet vel. Pellentesque sit amet euismod
+          justo. Sed eu mauris id nisi tristique molestie non vitae arcu. Ut
+          convallis at lectus a tempus. Nunc luctus elit et bibendum
+          sollicitudin. Curabitur sodales arcu quis pretium ultrices. In felis
+          metus, placerat facilisis sodales vestibulum, feugiat in lacus. Class
+          aptent taciti sociosqu ad litora torquent per conubia nostra, per
+          inceptos himenaeos. Morbi sodales neque eu aliquet cursus. Duis
+          elementum, magna nec efficitur mollis, enim augue auctor elit, nec
+          tincidunt velit tellus ac nisi. Aenean ac volutpat nisl. Maecenas
+          suscipit arcu id lacinia porttitor. Sed tempor metus nec mattis
+          vehicula. Vestibulum varius neque vel mi porta, a hendrerit nisl
+          auctor. Pellentesque efficitur eu sapien vulputate viverra. Aliquam
+          bibendum tincidunt dolor a porttitor. In id mollis lectus. Phasellus
+          consectetur, leo at semper dictum, enim mauris varius elit, vitae
+          sodales nibh dui a nibh. Vestibulum tristique sem felis, et aliquam
+          justo pellentesque eget. Vivamus tortor nisi, ultrices at metus at,
+          sollicitudin volutpat nunc.
+        </p>
+      </article>
+    </section>
+  );
+};
 
-export default Home
+export default Home;
