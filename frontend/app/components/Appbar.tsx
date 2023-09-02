b6b0570eae18502faf0945bfb5a686fede4b73f4
minh-p <minhisverynormal@gmail.com>
2023-09-02 16:43:12 -0700

    Home Hero section/subpage

--- frontend/app/components/Appbar.tsx
+++ frontend/app/components/Appbar.tsx
@@ -3,33 +3,47 @@ import SignInButton from '@/app/components/SignInButton'
 
 const Appbar = () => {
   return (
-    <nav className="w-screen flex flex-col lg:flex-row bg-red bg-[#952004] p-5 h-[179px]">
-      <div className="m-auto flex flex-col lg:flex-row space-x-14">
+    <nav className="w-screen flex flex-col xl:flex-row bg-red bg-[#952004] p-5 h-[179px]">
+      <div className="m-auto flex flex-col xl:flex-row space-x-14">
         <div>
           <ul className="flex flex-col">
-            <li className="text-4xl text-[#00a9ff]">Pacifica GGUSD</li>
-            <li className="text-4xl text-white">Corner Stone</li>
+            <li className="text-3xl xl:text-4xl text-[#00a9ff]">
+              Pacifica GGUSD
+            </li>
+            <li className="text-3xl xl:text-4xl text-white">Corner Stone</li>
           </ul>
         </div>
         <div className="flex flex-col space-y-4">
-          <ul className="flex flex-col lg:flex-row lg:space-x-10">
+          <ul className="flex flex-col xl:flex-row xl:space-x-10">
             <li>
-              <Link className="hover:underline text-4xl" href="/mission">
+              <Link
+                className="hover:underline text-3xl xl:text-4xl"
+                href="/mission"
+              >
                 Mission
               </Link>
             </li>
             <li>
-              <Link className="hover:underline text-4xl" href="/members">
+              <Link
+                className="hover:underline text-3xl text-3xl xl:text-4xl"
+                href="/members"
+              >
                 Members
               </Link>
             </li>
             <li>
-              <Link className="hover:underline text-4xl" href="/schedule">
+              <Link
+                className="hover:underline text-3xl text-3xl xl:text-4xl"
+                href="/schedule"
+              >
                 Schedule
               </Link>
             </li>
             <li>
-              <Link className="hover:underline text-4xl" href="/meeting">
+              <Link
+                className="hover:underline text-3xl xl:text-4xl"
+                href="/meeting"
+              >
                 Meeting
               </Link>
             </li>
@@ -37,7 +51,7 @@ const Appbar = () => {
               <SignInButton />
             </li>
           </ul>
-          <ul className="flex flex-col lg:flex-row lg:space-x-10">
+          <ul className="flex flex-col xl:flex-row xl:space-x-10">
             <li>
               <Link
                 className="hover:outline outline-2 bg-[#f40707] text-2xl p-1 rounded"
