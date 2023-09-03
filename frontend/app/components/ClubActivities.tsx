9d954c0618cb9bc1e0b6714744ee49edb3b3e1ca
minh-p <minhisverynormal@gmail.com>
2023-09-02 23:02:04 -0700

    Using proper youtube player instead.

--- frontend/app/components/ClubActivities.tsx
+++ frontend/app/components/ClubActivities.tsx
@@ -1,5 +1,22 @@
-import dynamic from 'next/dynamic'
-const ReactPlayer = dynamic(() => import('react-player/lazy'), { ssr: false })
+import YouTube, { YouTubeProps } from 'react-youtube'
+
+const YoutubeComponent = () => {
+  const onPlayerReady: YouTubeProps['onReady'] = (event) => {
+    // access to player in all event handlers via event.target
+    event.target.pauseVideo()
+  }
+
+  const opts: YouTubeProps['opts'] = {
+    height: '540',
+    width: '960',
+    playerVars: {
+      // https://developers.google.com/youtube/player_parameters
+      autoplay: 1,
+    },
+  }
+
+  return <YouTube videoId="JFwl-21pzQw" opts={opts} onReady={onPlayerReady} />
+}
 
 const ClubActivities = () => {
   // TODO Add editable data here from sanity.
@@ -8,13 +25,9 @@ const ClubActivities = () => {
       id="club-activities"
       className="relative max-w-[1920px] flex flex-col md:flex-row m-auto"
     >
-      <div className="text-center bg-white md:w-[50vw] aspect-video p-5 bg-slate-100">
-        <div className="m-auto">
-          <ReactPlayer
-            url="https://www.youtube.com/watch?v=JFwl-21pzQw"
-            width="100%"
-            height="522px"
-          />
+      <div className="text-center bg-white md:w-[50vw] aspect-video bg-slate-100">
+        <div className="m-auto p-5">
+          <YoutubeComponent />
         </div>
       </div>
       <div className="bg-[#f7f7f7] text-center md:w-[50vw] ">
