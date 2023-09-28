882ab39f8256b9b4fabf6a56390635a3193131c1
minh-p <minhisverynormal@gmail.com>
2023-09-28 16:28:44 -0700

    Added Preview of Meeting Notes on Home Page.

--- frontend/app/components/MeetingNotes.tsx
+++ frontend/app/components/MeetingNotes.tsx
@@ -1,3 +1,4 @@
+import { StopCircleIcon } from "@heroicons/react/24/solid"
 import { useSession } from 'next-auth/react'
 import Link from 'next/link'
 
@@ -10,10 +11,22 @@ const MeetingNotes = () => {
         id="meeting-notes"
         className="relative max-w-[1920px] m-auto h-[800px] bg-[#f9f5eb]"
       >
-        <div className="text-center pt-[50px]">
-          <h2 className="font-bold lx:text-5xl lg:text-4xl md:text-3xl text-2xl underline">
+        <div className="pt-[50px]">
+          <h2 className="text-center font-bold lx:text-5xl lg:text-4xl md:text-3xl text-2xl underline">
             Meeting Notes
           </h2>
+          <div className="md:max-w-[50%] max-w-[80%] text-2xl m-auto my-[50px] relative">
+            <div className="hidden absolute top-3 bottom-0 right-full mr-7 md:mr-[3.25rem] w-[3px] bg-slate-200 dark:bg-slate-800 sm:block">
+            </div>
+            <ul>
+              <li className="my-[25px] relative text-xl xl:text-2xl">
+                <StopCircleIcon className="bg-[#f9f5eb] hidden absolute mr-6 right-[99.5%] top-2 text-slate-200 dark:text-slate-600 md:mr-12 h-5 w-5 overflow-visible sm:block" />
+                <p>September 28th, 2023</p>
+                <p className="font-bold">First Meeting: Testing out the site.</p>
+                <p>Description: All Hail Ian Alves</p>
+              </li>
+            </ul>
+          </div>
           <Link
             href="/meetings"
             className="md:text-2xl no-underline bg-[#36406b] text-white p-2 rounded absolute bottom-[20px] left-[50px]"
@@ -27,12 +40,24 @@ const MeetingNotes = () => {
   return (
     <article
       id="meeting-notes"
-      className="relative max-w-[1920px] m-auto h-[800px] bg-[#f9f5eb]"
+      className="relative max-w-[1920px] m-auto h-[800px] bg-[#f9f5eb] p-5"
     >
-      <div className="text-center pt-[50px]">
-        <h2 className="font-bold lx:text-5xl lg:text-4xl md:text-3xl text-2xl underline">
+      <div className="pt-[50px]">
+        <h2 className="text-center font-bold lx:text-5xl lg:text-4xl md:text-3xl text-2xl underline">
           Meeting Notes
         </h2>
+        <div className="md:max-w-[50%] max-w-[80%] text-2xl m-auto my-[50px] relative">
+          <div className="hidden absolute top-3 bottom-0 right-full mr-7 md:mr-[3.25rem] w-[3px] bg-slate-200 dark:bg-slate-800 sm:block">
+          </div>
+          <ul>
+            <li className="my-[25px] relative text-xl xl:text-2xl">
+              <StopCircleIcon className="bg-[#f9f5eb] hidden absolute mr-6 right-[99.5%] top-2 text-slate-200 dark:text-slate-600 md:mr-12 h-5 w-5 overflow-visible sm:block" />
+              <p>September 28th, 2023</p>
+              <p className="font-bold">First Meeting: Testing out the site.</p>
+              <p>Description: All Hail Ian Alves</p>
+            </li>
+          </ul>
+        </div>
         <Link
           href="/api/auth/signin"
           className="md:text-2xl no-underline bg-[#36406b] text-white p-2 rounded absolute bottom-[20px] left-[50px]"
