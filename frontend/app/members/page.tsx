eb66812f680fcfc47da9234aca5daf0bd93c4324
minh-p <minhisverynormal@gmail.com>
2023-11-08 23:43:24 -0800

    Added Placeholder for 404 routes.

---
+++ frontend/app/members/page.tsx
@@ -0,0 +1,9 @@
+const Members = () => {
+  return (
+    <h1 className="text-center text-[26pt] md:text-[40pt]">
+      Nothing Here Yet &#128519;
+    </h1>
+  )
+}
+
+export default Members
---
+++ frontend/app/writing-press/page.tsx
@@ -0,0 +1,9 @@
+const WritingPress = () => {
+  return (
+    <h1 className="text-center text-[26pt] md:text-[40pt]">
+      Nothing Here Yet &#128519;
+    </h1>
+  )
+}
+
+export default WritingPress
